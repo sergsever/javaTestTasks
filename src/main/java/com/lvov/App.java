2482f2d997f5cd57610ce03da2d578e712e742a6
Sergey Lvov <sergey.lvov70@gmail.com>
2020-02-28 16:54:03 +0300

    Swap

--- src/main/java/com/lvov/App.java
+++ src/main/java/com/lvov/App.java
@@ -1,11 +1,35 @@
 package com.lvov;
 
+import org.apache.commons.lang.mutable.MutableInt;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    protected static class WrapInt
+    {
+        private int value;
+        public WrapInt(int value)
+        {
+            this.value = value;
+        }
+        
+        public int getValue(){
+            return value;
+        }
+        
+        public void setValue(int value)
+        {
+            this.value = value;
+        }
+        
+        public void Add(int number){
+            value += number;
+        }
+    }
+    
     public static void reverseArray(int[] array) {
         for (int i = 0, j = array.length - 1; i < j; ++i, --j) {
             int buff = array[j];
@@ -19,6 +43,16 @@ public class App
             int res = n < 2? 1 : fibonacci(n -2) + fibonacci(n - 1);
         return res;
     }
+    
+    public static void swapInt(WrapInt a, WrapInt b)
+    {
+        a.Add(b.getValue());
+        b.Add(- a.getValue());
+        b.setValue(- b.getValue());
+        a.Add(- b.getValue());
+        
+    }
+    
     public static void main( String[] args )
     {
         System.out.println( "test tasks\n" );
@@ -29,7 +63,10 @@ public class App
             System.out.println("i: " + i + "v: " + arr[i]);
         }
         
-        System.out.println("fibonacci7:" + fibonacci(7));
+        System.out.println("fibonacci 7:" + fibonacci(7));
+        WrapInt a = new WrapInt(5), b = new WrapInt(3);
+        swapInt(a, b);
+        System.out.println("a: " + a.getValue() + " b: " + b.getValue());
         
     }
 }
